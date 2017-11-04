db73f54a5e4e78bd7f9a53a756b97b0b579fb99c
nelson umunna <nelson.umunna@carleton.ca>
2017-11-04 11:33:16 -0400

    latest

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,7 +16,7 @@ public class Greeter {
   * @param someone the name of a person
   * @return greeting string
   */
-  // okay that's cool Nelson
+  // okay that's cool Nelson U
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
